abad5f5fb9504b598264bd097bbc4aa65282c688
testeducatorua <140176086+testeducatorua@users.noreply.github.com>
2024-11-13 19:51:46 +0200

    Update _app.js
    @vercel/speed-insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/global.css'
 
 export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+        <>
+          <Component {...pageProps} />
+          <SpeedInsights />
+        </>
+      );
 }
